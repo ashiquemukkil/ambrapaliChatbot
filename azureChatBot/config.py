254ecf149e745a86cb3c35a04f66aff823c64e7f
expert <ctpmukashi@gmail.com>
2020-08-18 22:54:06 +0530

    Initial commit

---
+++ azureChatBot/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = 'fb63aff1-dad0-4e3a-982c-9b21371600ee'
+    APP_PASSWORD = 'pass@123'
